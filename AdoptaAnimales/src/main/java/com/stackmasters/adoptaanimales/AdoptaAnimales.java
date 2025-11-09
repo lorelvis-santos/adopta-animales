fdf50279f745c39290e0e09ef5aa581c3eb5f44e
Lorelvis Santos <santoslorelvis@gmail.com>
2025-11-08 23:13:26 -0400

    first commit

---
+++ AdoptaAnimales/src/main/java/com/stackmasters/adoptaanimales/AdoptaAnimales.java
@@ -0,0 +1,7 @@
+package com.stackmasters.adoptaanimales;
+
+public class AdoptaAnimales {
+    public static void main(String[] args) {
+        System.out.println("¡StackMasters estuvo aquí!");
+    }
+}
