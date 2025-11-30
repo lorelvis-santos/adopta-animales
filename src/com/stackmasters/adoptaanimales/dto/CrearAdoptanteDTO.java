5811bfa1fe769fab259783ddd8d15b3b911cd978
Lorelvis Santos <santoslorelvis@gmail.com>
2025-11-30 03:54:11 -0400

    fix(solicitud): add missing CrearAdoptanteDTO

---
+++ src/com/stackmasters/adoptaanimales/dto/CrearAdoptanteDTO.java
@@ -0,0 +1,49 @@
+package com.stackmasters.adoptaanimales.dto;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Lorelvis Santos
+ */
+public class CrearAdoptanteDTO {
+    private final String nombre;
+    private final String apellido;
+    private final LocalDate fechaNacimiento;
+    private final String telefono;
+    private final String correo;
+    private final String direccion;
+
+    public CrearAdoptanteDTO(String nombre, String apellido, LocalDate fechaNacimiento, String telefono, String correo, String direccion) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    } 
+}
